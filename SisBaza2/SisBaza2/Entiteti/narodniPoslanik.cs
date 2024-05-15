47c1c0446944f7c9df3a2beea517ccdcf00dc54f
AleksaStankovic18485 <green.patan@elfak.rs>
2024-05-16 00:42:07 +0200

    Pocetak
    Pocetne stvari: paketi, folderi klase

---
+++ SisBaza2/SisBaza2/Entiteti/narodniPoslanik.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SisBaza2.Entiteti
+{
+    public class narodniPoslanik
+    {
+        public virtual int Id { get; protected set; }
+
+        public narodniPoslanik()
+        {
+
+        }
+    }
+    
+}
---
+++ SisBaza2/SisBaza2/Mapiranja/narodniPoslanikMapiranja.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using SisBaza2.Entiteti;
+
+namespace SisBaza2.Mapiranja
+{
+    class narodniPoslanikMapiranja : ClassMap<SisBaza2.Entiteti.narodniPoslanik>
+    {
+        public narodniPoslanikMapiranja()
+        {
+
+        }
+
+    }
+}
